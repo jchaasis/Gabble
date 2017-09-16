f2e6ced0c53341aac62141b095e33fe11848dbaa
jchaasis <jchaasis@gmail.com>
2017-09-16 14:07:02 -0400

    Add comments. presubmission commit.

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const bodyparser = require('body-parser');
 const Sequelize = require('sequelize');
 const session = require('express-session');
 
+//import schemas
 const schemas = require('./schemas/schemas.js');
 
 //Initialize app
-
 const app = express();
 
 //connect public folder
@@ -47,36 +47,38 @@ app.use(session({
   //home
   app.get('/home', function(req, res){
 
-  //likes list
-
+      //find all messages and include the user table
       schemas.Message.findAll({
               include: [ schemas.User ],
 
           }).then(function(messages){
-
+            //promises to be run
             const promises = [];
-
+            //check to see if the message author is the person
+            //currently logged in. If it is, set active to true so
+            // that they are able to delete a message and not able
+            //to like their own message, because liking your own
+            //post is super lame.
             for (let i = 0; i < messages.length; i++) {
               if (messages[i].userId === req.session.who.id){
                    messages[i].active = true;
-              }
+              } //count the number of likes each message has
               const promise = schemas.Like.count({
                 where: {
                   messageId: messages[i].id,
-                },
+                }, //store the number of likes for display
               }).then(function (likes) {
-                messages[i].likes = likes; // store the # of likes
+                messages[i].likes = likes;
               });
 
               promises.push(promise);
             }
-
+            //the promises and render the home page
             Promise.all(promises).then(function () {
               res.render('home', {
 
                       activename: req.session.who.name,
                       messages: messages,
-                      // active: active,
               });
             });
           });
@@ -85,69 +87,34 @@ app.use(session({
   //create
   app.get('/create', function(req, res){
     res.render('create');
-
   });
 
   //likes display page
   app.get('/likes/:message_id', function(req, res){
-
+        //store the param id se we can use it to render in the likes
+        //display page and also find the associated likes
         const id =  req.params.message_id;
-
-        // schemas.Message.findAll({
-        //         include: [ schemas.User ],
-        //
-        //     }).then(function(messages){
-        //
-        //       const promises = [];
-        //
-        //       for (let i = 0; i < messages.length; i++) {
-        //         const promise = schemas.Like.count({
-        //           where: {
-        //             messageId: messages[i].id,
-        //           },
-        //         }).then(function (likes) {
-        //           messages[i].likes = likes; // store the # of likes
-        //         });
-        //
-        //         promises.push(promise);
-        //       }
-        //
-        //       Promise.all(promises).then(function () {
-        //         res.render('home', {
-        //                 activename: req.session.who.name,
-        //                 messages: messages,
-        //         });
-        //       });
-        //     });
-
+        //find all likes with the corresponding message id
         schemas.Like.findAll({
           where: {messageId: parseInt(id)},
           include: [ schemas.User ],
-
         }
       ).then(function(results){
+          //find the message with the corresponding message id
+          schemas.Message.findById(parseInt(id)).then(function(message){
 
-        schemas.Message.findById(parseInt(id)).then(function(message){
+            res.render("likes", {
+                message: message,
+                results: results,
 
-          res.render("likes", {
-              message: message,
-              results: results,
-
-          });
-
-        })
-
-        });
+            });
+          })
+      });
   });
-
 
 //post
   //new user
   app.post('/register', function(req, res){
-
-  //TODO: check to see if the username already exists
-  //      validate the password.
-  //      create new session directly from registration
 
     schemas.User.create({
       name: req.body.new_name,
@@ -161,22 +128,23 @@ app.use(session({
   app.post('/login', function(req, res){
       schemas.User.findAll().then( function(users){
         for (let i = 0; i < users.length; i++){
+          //check to see if the username & password combo typed in is
+          //a registered user. If they are, direct them to the home page and
+          //create a new session
           if (req.body.username === users[i].username &&
               req.body.password === users[i].password){
                 req.session.who = users[i];
                 req.session.who.name = users[i].name;
                 req.session.who.id = users[i].id;
                 res.redirect('/home');
-              } else {
-                // res.redirect('/login');
-              }
+          }
         }
       });
   });
 
   //create Gab
   app.post('/create', function(req, res){
-
+      //create new message
       schemas.Message.create({
         body: req.body.gabBody,
         userId: req.session.who.id,
@@ -187,14 +155,16 @@ app.use(session({
   });
   //delete Gab
   app.post('/delete/:id', function(req, res){
-
+    //first delete the likes associated with the messageId
+    //so that we can delete the message
     schemas.Like.destroy({
                   where: {
                     messageId: req.params.id,
                   }
 
     }).then(function(){
-
+      //now that the likes are gone, the message is not connected to any
+      //other tables so we can delete it.
       schemas.Message.destroy({
                       where: {
                         id: req.params.id,
@@ -203,21 +173,12 @@ app.use(session({
           res.redirect('/home');
       });
     });
-
   });
-  //
-  //   schemas.Message.destroy({
-  //                   where: {
-  //                     id: req.params.id,
-  //                   }
-  //   }).then(function(){
-  //       res.redirect('/home');
-  //   });
-  // });
 
   //like a message
   app.post('/like/:id', function(req, res){
-
+    //find the message the user wishes to like and add a like
+    //to the likes table with the associated messageId and UserId
     schemas.Message.findById(req.params.id,)
         .then(function (liked) {
           schemas.Like.create({
@@ -234,12 +195,10 @@ app.use(session({
 
   //logout
   app.post('/logout', function(req, res){
-
+    //destroy session and logout
     req.session.destroy();
     res.redirect('/login');
-
   });
-
 
 //ready, set, go
 app.listen(4500, function(){
--- schemas/schemas.js
+++ schemas/schemas.js
@@ -22,7 +22,6 @@ const Sequelize = require('sequelize');
 
     });
 
-
     User.hasMany(Message, {foreignkey: 'userId'});
     User.hasMany(Like, {foreignkey: 'userId'});
     Message.belongsTo(User, {foreignKey: 'userId'});
@@ -33,51 +32,10 @@ const Sequelize = require('sequelize');
     Message.sync();
     Like.sync();
 
-      //  Message.belongsTo(User);
-      //  Like.belongsTo(Message);
-      //  Like.belongsTo(User);
-
-      // User.hasMany(Message)
-      //
-      //  Message.belongsTo(User, {foreignKey: 'userId'});
-      //  Like.belongsTo(Message, {foreignKey: 'messageId'});
-      //  Like.belongsTo(User, {foreignKey: 'userId'});
-
-    //
-    // // Sychronize the schemas with the database, meaning make
-    // // sure all tables exist and have the right fields.
-    //
-    // return User.sync()
-    //     .then(() => Message.sync())
-    //     .then(() => Like.sync())
-    //     .then(() => {
-    //         return { User, Message, Like };
-    //     });
-
-        //  User.sync().then(function () {
-        //     console.log('user model syncd');
-        //  });
-         //
-        //  Message.sync().then(function () {
-        //     console.log('message model syncd');
-        //  });
-         //
-        //  Like.sync().then(function () {
-        //     console.log('like model syncd');
-        //  });
-
-        // Todo.create({
-        //      item: 'Create todo list',
-        //      complete: false,
-        // });
-    // });
-
-// };
 // ************* Schema End **********************
 
 module.exports = {
   User: User,
   Message: Message,
   Like: Like,
-
 }
